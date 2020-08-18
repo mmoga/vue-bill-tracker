2d2b4169f39b7d2391170bb61ca7387525da1835
Matthew <31258808+mmoga@users.noreply.github.com>
2020-08-18 13:05:21 -0400

    I guess I can add bill amounts now :shrug:

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,35 @@
   <main>
     <AddCategory v-if="shouldShowAddCategory" v-on:addCategory="addCategory"/>
     <div v-else>
-      <NavBar :categories="categories" v-on:triggerShowAddCategory="triggerShowAddCategory"/>
-      <div class="container flex">
-        <div class="w-1/2">
-          <BillsTable/>
-        </div>
-        <div class="w-1/2">
-          <Chart :bills="activeBills"/>
+      <AddBill v-if="shouldShowAddBill" :categories="categories" v-on:addBill="addBill"/>
+      <div v-else>
+        <NavBar :categories="categories" v-on:triggerShowAddCategory="triggerShowAddCategory"/>
+        <div class="container flex">
+          <div class="w-1/2">
+            <BillsTable :bills="bills" v-on:triggerShowAddBill="triggerShowAddBill"/>
+          </div>
+          <div class="w-1/2">
+            <Chart/>
+          </div>
         </div>
       </div>
     </div>
   </main>
 </template>
 
+
 <script>
 import Vue from "vue";
+Vue.use(require("vue-moment"));
 
 import AddCategory from "./components/AddCategory.vue";
 import AddBill from "./components/AddBill.vue";
+import NavBar from "./components/NavBar.vue";
 import Chart from "./components/Chart.vue";
 import BillsTable from "./components/BillsTable.vue";
-import NavBar from "./components/NavBar.vue";
 
 export default {
   name: "app",
-
   components: {
     AddCategory,
     AddBill,
@@ -38,7 +42,8 @@ export default {
     return {
       bills: [],
       categories: [],
-      shouldShowAddCategory: true
+      shouldShowAddCategory: false,
+      shouldShowAddBill: true
     };
   },
   methods: {
@@ -48,22 +53,35 @@ export default {
     },
     triggerShowAddCategory() {
       this.shouldShowAddCategory = true;
+    },
+    addBill(bill) {
+      this.bills.push(bill);
+      this.shouldShowAddBill = false;
+    },
+    triggerShowAddBill() {
+      this.shouldShowAddBill = true;
     }
   },
   watch: {
+    bills() {
+      localStorage.setItem("bills", JSON.stringify(this.bills));
+    },
     categories() {
       localStorage.setItem("categories", JSON.stringify(this.categories));
     }
   },
   mounted() {
+    if (localStorage.getItem("bills")) {
+      this.bills = JSON.parse(localStorage.getItem("bills"));
+    }
+
     if (localStorage.getItem("categories")) {
       this.categories = JSON.parse(localStorage.getItem("categories"));
     }
 
-    if (!this.categories.length) {
+    if (!this.bills.length && !this.categories.length) {
       this.shouldShowAddCategory = true;
     }
   }
 };
 </script>
-
